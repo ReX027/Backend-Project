46d4b6c47c76132dc94f1e7090b3854661f68d7e
Tushar <tushar.vaid027@gmail.com>
2023-12-17 01:21:18 +0530

    MongoDB connection Done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("HI")
+// require('dotenv').config({path: './env'});
+import dotenv from "dotenv"
+import connecToDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connecToDB()
