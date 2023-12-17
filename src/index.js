b07baf899628c5e5322133b57db1dcd08a67d473
Tushar <tushar.vaid027@gmail.com>
2023-12-17 16:37:59 +0530

    Utils code

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@ dotenv.config({
     path: './env'
 })
 
-connecToDB()
+connecToDB() //async function will return response so now using .then and .catch
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error: ",error);
+        throw error
+    })
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`App running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed",err);
+})
